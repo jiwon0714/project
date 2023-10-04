38a00606270a785545d5b5f51d370015ff05e336
jiwon0714 <star13535@naver.com>
2023-10-05 03:09:11 +0900

    프로젝트 sns 화면 구성

--- app/src/main/java/com/example/myapplication/SNSActivity.java
+++ app/src/main/java/com/example/myapplication/SNSActivity.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SNSActivity extends AppCompatActivity {
 
+    private ImageButton home, heart, chat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snsactivity);
+
+
+        home = findViewById(R.id.home);
+        heart = findViewById(R.id.heart);
+        chat = findViewById(R.id.chat);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SNSActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
