78b32ff95748517a5246acd4ed1e89b137668439
pcw999 <cksdnelly@gmail.com>
2023-11-06 22:19:55 +0900

    use retrofit

--- app/src/main/java/com/example/myapplication/controller/Api.java
+++ app/src/main/java/com/example/myapplication/controller/Api.java
@@ -1,15 +1,18 @@
 package com.example.myapplication.controller;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.Part;
 
 import com.example.myapplication.dto.post;
+import com.example.myapplication.dto.post_upload;
+
 public interface Api {
     @GET("sample_post")
     Call<post> getData();
 
-    //@POST("upload_post")
+    @POST("upload_post")
+    Call <post_upload> postData(@Body post_upload post);
     
 }
 
