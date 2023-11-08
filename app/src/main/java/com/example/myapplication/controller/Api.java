66ad338ed7d8337bbac1ffa30c106af3d499b324
SuHyeon1592 <bdt1592@naver.com>
2023-11-08 18:10:06 +0900

    image upload

--- app/src/main/java/com/example/myapplication/controller/Api.java
+++ app/src/main/java/com/example/myapplication/controller/Api.java
@@ -1,18 +1,18 @@
 package com.example.myapplication.controller;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
+import com.example.myapplication.dto.ImageDTO;
 import com.example.myapplication.dto.post;
-import com.example.myapplication.dto.post_upload;
 
 public interface Api {
     @GET("sample_post")
     Call<post> getData();
 
-    @POST("upload_post")
-    Call <post_upload> postData(@Body post_upload post);
-    
+    @POST("upload")
+    Call <ResponseBody> postImage(@Body ImageDTO imageDTO);
 }
 
