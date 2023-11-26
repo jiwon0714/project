17436f1eaaa8e54928ccb1b1eaa40cc859fee987
jiwon0714 <star13535@naver.com>
2023-11-26 22:30:22 +0900

    11/26 뷰홀더

--- app/src/main/java/com/example/myapplication/chat/ChatAdapter.java
+++ app/src/main/java/com/example/myapplication/chat/ChatAdapter.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.chat;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,33 +14,58 @@ import com.example.myapplication.R;
 import java.util.List;
 
 // ChatAdapter.java
-public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
+public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<ChatMessage> messages;
+
+    // 뷰 타입 상수 정의
+    private static final int VIEW_TYPE_SENT = 1;
+    private static final int VIEW_TYPE_RECEIVED = 2;
 
     public ChatAdapter(List<ChatMessage> messages) {
         this.messages = messages;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        // 해당 아이템의 뷰 타입을 반환
+        return messages.get(position).isSent() ? VIEW_TYPE_SENT : VIEW_TYPE_RECEIVED;
+    }
+
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.chat_list_talk_item_mine, parent, false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // 뷰 타입에 따라 다른 뷰홀더를 생성
+        if (viewType == VIEW_TYPE_SENT) {
+            View sentView = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_list_talk_item_mine, parent, false);
+            return new SentMessageViewHolder(sentView);
+        } else {
+            View receivedView = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_list_talk_item_mine, parent, false);
+            return new ReceivedMessageViewHolder(receivedView);
+        }
+        //return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         ChatMessage message = messages.get(position);
 
-        holder.txtMessage.setText(message.getMessageContent());
-        holder.txtDate.setText(message.getTimestamp());
+//        holder.txtMessage.setText(message.getMessageContent());
+//        holder.txtDate.setText(message.getTimestamp());
+
+        if (holder instanceof SentMessageViewHolder) {
+            ((SentMessageViewHolder) holder).bind(message);
+        } else if (holder instanceof ReceivedMessageViewHolder) {
+            ((ReceivedMessageViewHolder) holder).bind(message);
+        }
 
         // 읽음 상태에 따라 UI 업데이트
         if (message.isRead()) {
-            holder.txtIsShown.setVisibility(View.GONE);
+            holder.itemView.setVisibility(View.GONE);
         } else {
-            holder.txtIsShown.setVisibility(View.VISIBLE);
+            holder.itemView.setVisibility(View.VISIBLE);
         }
     }
     @Override
@@ -52,18 +78,60 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
         notifyDataSetChanged();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    // 발신 메시지 뷰홀더
+    private static class SentMessageViewHolder extends RecyclerView.ViewHolder {
         TextView txtMessage;
         TextView txtDate;
         TextView txtIsShown;
 
+        SentMessageViewHolder(View itemView) {
+            super(itemView);
+            txtMessage = itemView.findViewById(R.id.txt_message);
+            txtDate = itemView.findViewById(R.id.txt_date);
+            txtIsShown = itemView.findViewById(R.id.txt_isShown); // 초기화 추가
 
-        public ViewHolder(View view) {
-            super(view);
-            txtMessage = view.findViewById(R.id.txt_message);
-            txtDate = view.findViewById(R.id.txt_date);
-            txtIsShown = view.findViewById(R.id.txt_isShown); // 초기화 추가
+        }
+
+        void bind(ChatMessage message) {
+            txtMessage.setText(message.getMessageContent());
+            txtDate.setText(message.getTimestamp());
 
         }
     }
+
+    // 수신 메시지 뷰홀더
+    private static class ReceivedMessageViewHolder extends RecyclerView.ViewHolder {
+        TextView txtMessage;
+        TextView txtDate;
+        TextView txtIsShown;
+
+        ReceivedMessageViewHolder(View itemView) {
+            super(itemView);
+            txtMessage = itemView.findViewById(R.id.txt_message);
+            txtDate = itemView.findViewById(R.id.txt_date);
+            txtIsShown = itemView.findViewById(R.id.txt_isShown); // 초기화 추가
+
+        }
+
+        void bind(ChatMessage message) {
+            txtMessage.setText(message.getMessageContent());
+            txtDate.setText(message.getTimestamp());
+
+        }
+    }
+
+//    public static class ViewHolder extends RecyclerView.ViewHolder {
+//        TextView txtMessage;
+//        TextView txtDate;
+//        TextView txtIsShown;
+//
+//
+//        public ViewHolder(View view) {
+//            super(view);
+//            txtMessage = view.findViewById(R.id.txt_message);
+//            txtDate = view.findViewById(R.id.txt_date);
+//            txtIsShown = view.findViewById(R.id.txt_isShown); // 초기화 추가
+//
+//        }
+//    }
 }
--- app/src/main/java/com/example/myapplication/chat/ChatMessage.java
+++ app/src/main/java/com/example/myapplication/chat/ChatMessage.java
@@ -4,11 +4,17 @@ public class ChatMessage {
     private String messageContent;
     private String timestamp;
     private boolean isRead; // 읽음 상태를 나타내는 필드 추가
+    private boolean isSent; // 발신 여부를 나타내는 필드 추가
+
+    private int viewType;
 
     public ChatMessage(String messageContent, String timestamp) {
         this.messageContent = messageContent;
         this.timestamp = timestamp;
         this.isRead = false; // 초기값은 읽지 않음
+        this.isSent = isSent;
+        this.viewType = viewType;
+
     }
 
     public String getMessageContent() {
@@ -22,8 +28,13 @@ public class ChatMessage {
     public boolean isRead() {
         return isRead;
     }
+    public boolean isSent() {return isSent;}
 
     public void setRead(boolean read) {
         isRead = read;
+    }
+
+    public int getViewType() {
+        return viewType;
     }
 }
