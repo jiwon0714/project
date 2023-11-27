18176b7ec0ef976f41ddb9217636bbb5bba65904
jiwon0714 <star13535@naver.com>
2023-11-28 03:24:47 +0900

    11/28

---
+++ app/src/main/java/com/example/myapplication/sns/Comment_Item.java
@@ -0,0 +1,42 @@
+package com.example.myapplication.sns;
+
+public class Comment_Item {
+
+//    private Bitmap profileImage;
+//    private String name;
+    private String commentContext;
+    private String commentTime;
+    private boolean isHeart;
+    private int heartCount;
+
+    public Comment_Item( String commentContext, String commentTime, boolean isHeart, int heartCount) {
+//        this.profileImage = profileImage;
+//        this.name = name;
+        this.commentContext = commentContext;
+        this.commentTime = commentTime;
+        this.isHeart = isHeart;
+        this.heartCount = heartCount;
+    }
+
+
+    //    public Bitmap getProfileImage(){return profileImage;}
+//    public String getname() {
+//        return name;
+//    }
+    public String getcommentContext() {
+        return commentContext;
+    }
+    public String getcommentTime() {
+        return commentTime;
+    }
+    public boolean isHeart() {return isHeart;}
+    public int getHeartCount(){return heartCount;}
+
+    public void setHeartCount(int heartCount) {
+        this.heartCount = heartCount;
+    }
+
+    public void setHeart(boolean heart) {
+        isHeart = heart;
+    }
+}
--- app/src/main/java/com/example/myapplication/sns/SnsRecyclerAdapter.java
+++ app/src/main/java/com/example/myapplication/sns/SnsRecyclerAdapter.java
@@ -1,21 +1,44 @@
 package com.example.myapplication.sns;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class SnsRecyclerAdapter extends RecyclerView.Adapter<SnsRecyclerAdapter.ViewHolder> {
     private ArrayList<SnsItem> mSnsList;
+    private ArrayList<Comment_Item> commentList;
+
+    private EditText et_comment;
+
+    private CommentAdapter adapter;
+
+    int click = 0;
+    int numofheart = 0;
 
     @NonNull
     @Override
@@ -27,6 +50,137 @@ public class SnsRecyclerAdapter extends RecyclerView.Adapter<SnsRecyclerAdapter.
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.onBind(mSnsList.get(position));
+
+        holder.heart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(click ==0) {
+                    holder.heart.setImageResource(R.drawable.fillheart);
+                    numofheart++;
+                    holder.heartCounter.setText("좋아요 " + numofheart + "개");
+                    click ++;
+                }else{
+                    holder.heart.setImageResource(R.drawable.heart);
+                    --numofheart;
+                    holder.heartCounter.setText("좋아요 " + numofheart + "개");
+                    click --;
+                }
+            }
+        });
+
+
+        //댓글창
+        holder.chat_to_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // AlertDialog 빌더 생성
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+
+
+                // View를 인플레이트할 때 holder.itemView.getContext()를 사용
+                View dialogView = LayoutInflater.from(holder.itemView.getContext()).inflate(R.layout.dialog_sns_comment, null);
+
+                // AlertDialog에 리사이클러뷰 추가
+                RecyclerView recyclerView = dialogView.findViewById(R.id.recyclerView);
+                LinearLayoutManager layoutManager = new LinearLayoutManager(holder.itemView.getContext());
+                recyclerView.setLayoutManager(layoutManager);
+
+                // 리사이클러뷰 어댑터 설정
+                commentList = new ArrayList<>();
+
+                adapter = new CommentAdapter(commentList);
+                recyclerView.setAdapter(adapter);
+
+                // AlertDialog 생성 및 표시
+                AlertDialog alertDialog = builder.create();
+                alertDialog.setView(dialogView);  // AlertDialog에 설정한 뷰를 세팅
+
+                // 커스텀 애니메이션 적용
+                Window window = alertDialog.getWindow();
+
+                // AlertDialog의 크기 및 위치 조절
+                alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface dialog) {
+                        // AlertDialog의 크기 조절
+                        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+                        layoutParams.copyFrom(alertDialog.getWindow().getAttributes());
+                        layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+
+                        // 세로 길이를 화면의 절반으로 설정
+                        layoutParams.height = (int) (holder.itemView.getContext().getResources().getDisplayMetrics().heightPixels * 0.6);
+
+                        // AlertDialog를 하단에 위치시키고 여백 없이 설정
+                        layoutParams.gravity = Gravity.BOTTOM | Gravity.FILL_HORIZONTAL;
+
+
+
+                        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                        if (window != null) {
+                            // 열기&닫기 시 애니메이션 설정
+                            layoutParams.windowAnimations = R.style.AnimationPopupStyle;
+                            window.setAttributes( layoutParams );
+                            // dim 처리
+                            window.setDimAmount(0.7f); // 원하는 dim 정도로 조절
+                        }
+
+                        alertDialog.getWindow().setAttributes(layoutParams);
+                    }
+                });
+
+                Button btn_comment = dialogView.findViewById(R.id.btn_comment);
+                et_comment = dialogView.findViewById(R.id.et_comment);
+
+
+                btn_comment.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sendMessage();
+                    }
+                });
+
+                et_comment.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                    @Override
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        if (actionId == EditorInfo.IME_ACTION_SEND) {
+                            sendMessage();
+                            return true;
+                        }
+                        return false;
+                    }
+                });
+
+
+                alertDialog.show();
+            }
+        });
+    }
+
+    private void sendMessage() {
+        String commentContext = et_comment.getText().toString();
+
+        if (!commentContext.isEmpty()) {
+            String commentTime = getCurrentTimestamp();
+
+            // 프로필 이미지와 이름을 가져와서 Comment_Item을 생성하는 부분(db에서 정보 받아와서)
+            // CircleImageView profileImage = ...
+            // String name = ...
+
+            Comment_Item newComment = new Comment_Item(commentContext, commentTime,false, 0);
+
+            // 어댑터에 메시지 추가
+            adapter.addComment(newComment);
+            adapter.notifyDataSetChanged(); // 댓글이 추가되었음을 어댑터에 알림
+
+            // 입력 필드 비우기
+            et_comment.setText("");
+        }
+    }
+
+    private String getCurrentTimestamp() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        return sdf.format(new Date());
     }
 
     public void setSnsList(ArrayList<SnsItem> list){
@@ -49,7 +203,13 @@ public class SnsRecyclerAdapter extends RecyclerView.Adapter<SnsRecyclerAdapter.
         TextView name;
         TextView text;
         TextView comment;
+        ImageView heart;
+
         TextView comment_name;
+        ImageView chat_to_comment;
+        TextView heartCounter;
+
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -57,10 +217,14 @@ public class SnsRecyclerAdapter extends RecyclerView.Adapter<SnsRecyclerAdapter.
             comment_name = (TextView) itemView.findViewById(R.id.comment_name);
             comment = (TextView) itemView.findViewById(R.id.comment);
             main_image = (ImageView) itemView.findViewById(R.id.main_image);
+            chat_to_comment = (ImageView) itemView.findViewById(R.id.chat);
+            heart = (ImageView) itemView.findViewById(R.id.heart);
 
             profile = (ImageView) itemView.findViewById(R.id.img_profile);
             name = (TextView) itemView.findViewById(R.id.tv_profile);
             text = (TextView) itemView.findViewById(R.id.sns_main);
+            heartCounter = (TextView)  itemView.findViewById(R.id.favoritecounter);
+
         }
 
         void onBind(SnsItem item){
